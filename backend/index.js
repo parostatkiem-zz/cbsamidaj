81d197d07e6c224f4e1908f1169c4df450bb0912
Wawrzyn321 <Wawrzyn321@gmail.com>
2020-10-29 12:33:37 +0100

    pods

--- backend/index.js
+++ backend/index.js
@@ -19,25 +19,33 @@ kubeconfig.loadFromFile(process.env.KUBECONFIG);
 
 new SubscriptionPool(io, kubeconfig);
 
-const apiRulesUrl = `${kubeconfig.getCurrentCluster().server
-  }/apis/gateway.kyma-project.io/v1alpha1/namespaces/default/apirules/`;
 
-app.delete("/api-rules", async (req, res) => {
-  const { name } = req.query;
+// app.delete("/api-rules", async (req, res) => {
+//   const { name } = req.query;
+//   const opts = {};
+//   kubeconfig.applyToRequest(opts);
+//   request.delete(`${apiRulesUrl}${name}`, opts, (error, response, body) => {
+//     if (error) console.log(error);
+//     res.sendStatus(200);
+//   });
+// // });
+
+// console.log(kubeconfig.getUsers()[1].exec.args)
+// console.log(kubeconfig.getUsers()[1].exec.args.join(' '))
+// const currentContext = kubeconfig.getContexts()
+//   .find(c => c.name === kubeconfig.getCurrentContext());
+//   console.log(currentContext)
+app.get("/pods", async (req, res) => {
+  const ns = ['kyma-system', 'kube-system', 'default', 'kyma-installer'][Math.ceil(Math.random()*4)]
+
+
+  const podsUrl = `${kubeconfig.getCurrentCluster().server
+    }/api/v1/namespaces/${ns}/pods/`;
+  
+    
   const opts = {};
   kubeconfig.applyToRequest(opts);
-  request.delete(`${apiRulesUrl}${name}`, opts, (error, response, body) => {
-    if (error) console.log(error);
-    res.sendStatus(200);
-  });
-});
-
-app.get("/api-rules", async (req, res) => {
-  const opts = {};
-  kubeconfig.applyToRequest(opts);
-
-  request.get(apiRulesUrl, opts, (error, response, body) => {
-
+  request.get(podsUrl, opts, (error, response, body) => {
     // todo return {data: data, error: error}
     if (error) {
       res.send(error);
@@ -47,32 +55,32 @@ app.get("/api-rules", async (req, res) => {
   });
 });
 
-app.patch("/api-rules", async (req, res) => {
-  const { name, namespace, apiRule } = req.query;
+// app.patch("/api-rules", async (req, res) => {
+//   const { name, namespace, apiRule } = req.query;
 
-  const parameterizedApiRulesUrl = `${kubeconfig.getCurrentCluster().server
-    }/apis/gateway.kyma-project.io/v1alpha1/namespaces/${namespace}/apirules/${name}`;
+//   const parameterizedApiRulesUrl = `${kubeconfig.getCurrentCluster().server
+//     }/apis/gateway.kyma-project.io/v1alpha1/namespaces/${namespace}/apirules/${name}`;
 
-  const opts = {};
-  kubeconfig.applyToRequest(opts);
-  request.get(parameterizedApiRulesUrl, opts, (error, response, body) => {
-    const data = {
-      url: parameterizedApiRulesUrl,
-      body: apiRule ,
-      rejectUnauthorized: false,
-      headers: { "Content-type": 'application/merge-patch+json' },
-    };
-    kubeconfig.applyToRequest(data);
+//   const opts = {};
+//   kubeconfig.applyToRequest(opts);
+//   request.get(parameterizedApiRulesUrl, opts, (error, response, body) => {
+//     const data = {
+//       url: parameterizedApiRulesUrl,
+//       body: apiRule ,
+//       rejectUnauthorized: false,
+//       headers: { "Content-type": 'application/merge-patch+json' },
+//     };
+//     kubeconfig.applyToRequest(data);
 
-    request.patch(data, (error, response, body) => {
-      if (error) {
-        res.send(error);
-      } else {
-        res.send(JSON.parse(body));
-      }
-    });
-  });
-});
+//     request.patch(data, (error, response, body) => {
+//       if (error) {
+//         res.send(error);
+//       } else {
+//         res.send(JSON.parse(body));
+//       }
+//     });
+//   });
+// });
 
 const port = process.env.PORT || 3001;
 const address = process.env.ADDRESS || "localhost";
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,88 +1,44 @@
 import React from "react";
 import { useGet, useDelete, useUpdate, useSubscription } from "./api";
 import { handleSubscriptionEvent } from "./handleSubscriptionEvent";
-import { ApiRuleDetailsForm } from "./ApiRuleDetailsForm/ApiRuleDetailsForm";
 
-
-function ApiRuleContent({ metadata, spec, status, deleteApiRule, updateApiRule }) {
-
-  const [edit, setEdit] = React.useState(false);
-  return (
-    <details key={metadata.name}>
-      <summary>
-        {metadata.name} @ {metadata.namespace} [{status?.APIRuleStatus.code}]
-        <button onClick={() => deleteApiRule(metadata)}>Delete</button>
-        {!edit && <button onClick={() => setEdit(true)}>Edit</button>}
-        {edit && <button onClick={() => setEdit(false)}>Cancel</button>}
-      </summary>
-      {!edit &&
-        <ApiRuleDetails spec={spec} />}
-      {edit &&
-        <ApiRuleDetailsForm metadata={metadata} spec={spec} edit={edit} updateApiRule={updateApiRule} />}
-    </details>
-  );
-}
-function ApiRuleDetails({ spec }) {
-  return (
-    <dl>
-      <dt>Gateway</dt>
-      <dd>{spec.gateway}</dd>
-      <dt>Service Host</dt>
-      <dd>{spec.service.host}</dd>
-      <dt>Rules</dt>
-      <ul>
-        {spec.rules.map((r) => (
-          <li key={r.path}>
-            <p>{r.path}</p>
-            <dl>
-              <dt>Type</dt>
-              <dd>{r.accessStrategies[0].handler}</dd>
-              <dt>Methods</dt>
-              <dd>{r.methods.join(", ")}</dd>
-            </dl>
-          </li>
-        ))}
-      </ul>
-    </dl>
-  );
-}
 
 export default function App() {
-  const [apiRules, setApiRules] = React.useState([]);
-  const { loading, error } = useGet("api-rules", setApiRules);
-  const updateApiRuleMutation = useUpdate("api-rules");
-  const deleteApiRuleMutation = useDelete("api-rules");
+  const [pods, setPods] = React.useState([]);
+  const { loading, error } = useGet("pods", setPods);
+  // const updateApiRuleMutation = useUpdate("pods");
+  const deleteApiRuleMutation = useDelete("pods");
 
-  useSubscription(
-    "api-rules",
-    React.useCallback(handleSubscriptionEvent(setApiRules), [])
-  );
+  // useSubscription(
+  //   "pods",
+  //   React.useCallback(handleSubscriptionEvent(setPods), [])
+  // );
 
-  const deleteApiRule = async ({ name, namespace }) => {
-    try {
-      await deleteApiRuleMutation({ name, namespace });
-    } catch (e) {
-      console.warn(e);
-    }
-  };
+  // const deleteApiRule = async ({ name, namespace }) => {
+  //   try {
+  //     await deleteApiRuleMutation({ name, namespace });
+  //   } catch (e) {
+  //     console.warn(e);
+  //   }
+  // };
 
-  const updateApiRule = async ({ name, namespace, apiRule }) => {
-    try {
-      await updateApiRuleMutation({ name, namespace, apiRule });
-    } catch (e) {
-      console.warn(e);
-    }
-  };
+  // const updateApiRule = async ({ name, namespace, apiRule }) => {
+  //   try {
+  //     await updateApiRuleMutation({ name, namespace, apiRule });
+  //   } catch (e) {
+  //     console.warn(e);
+  //   }
+  // };
 
 
   if (loading) return "Loading...";
   if (error) return error.message;
-  if (!apiRules.length) return "Hm, pusto";
+  if (!pods.length) return "Hm, pusto";
 
   return (
     <main>
-      {apiRules.map(({ metadata, spec, status }) => (
-        <ApiRuleContent metadata={metadata} spec={spec} status={status} deleteApiRule={deleteApiRule} updateApiRule={updateApiRule} />
+      {pods.map(({ metadata }) => (
+        <div key={metadata.name}>{metadata.name}</div>
       ))}
     </main>
   );
