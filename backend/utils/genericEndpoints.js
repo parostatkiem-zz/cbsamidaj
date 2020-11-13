93dd4fdf0cbea902c52b38c1c000e49baae6f4ba
parostatkiem <jan.sudczak@sap.com>
2020-11-13 09:29:11 +0100

    rework update mutation

--- backend/utils/genericEndpoints.js
+++ backend/utils/genericEndpoints.js
@@ -1,6 +1,5 @@
 import injectTokenToOptions from "./tokenInjector";
 import fetch from "node-fetch";
-import { loadYaml, CoreV1Api, dumpYaml } from "@kubernetes/client-node";
 
 function addJsonFieldToItems(sourceJSON, extraHeader) {
   //TODO do this in a better way
@@ -48,22 +47,35 @@ export const createGenericGetEndpoint = (kubeconfig, app) => (
   });
 };
 
-export const createGenericJsonUpdateEndpoint = (kubeconfig, app) => (path) => {
+export const createGenericJsonUpdateEndpoint = (kubeconfig, app) => (
+  path,
+  resourceUrlFragments,
+  isNamespaced = true
+) => {
   app.patch(path, async (req, res) => {
     const { name, namespace, json } = req.body;
-    const opts = injectTokenToOptions({}, req, kubeconfig);
-    const u = kubeconfig.getCurrentUser();
-    u.token = opts.headers.Authorization.slice(7, opts.headers.Authorization.length);
-    kubeconfig.users = [u];
-    const k8sApi = kubeconfig.makeApiClient(CoreV1Api);
+
+    const opts = injectTokenToOptions(
+      {
+        rejectUnauthorized: false,
+        body: JSON.stringify(json),
+        headers: {
+          "content-type": "application/json",
+        },
+      },
+      req,
+      kubeconfig
+    );
 
     try {
-      const responseJSON = await k8sApi.replaceNamespacedPod(name, namespace, json);
-      // console.log(responseJSON);
-      res.send(responseJSON.response.request);
+      const url = calculateURL([...resourceUrlFragments], isNamespaced, namespace) + "/" + name; //todo
+
+      const response = await fetch(url, { method: "PUT", ...opts });
+      res.send(response);
     } catch (e) {
-      res.status(e.response?.body?.code || 500);
-      res.send(e.response?.body?.message || e.message);
+      console.error(e);
+      res.status(500);
+      res.send(e.message);
     }
   });
 };
