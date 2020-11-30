62ca97c72d61166f1deb55d1c700d8ecbcaf82ed
parostatkiem <jan.sudczak@sap.com>
2020-11-30 10:37:43 +0100

    minor clean up

--- backend/index.js
+++ backend/index.js
@@ -20,31 +20,13 @@ createPodEndpoints(kubeconfig, app);
 
 new SubscriptionPool(io, kubeconfig, app);
 
-initializeApp(app, kubeconfig); //todo
+initializeApp(app, kubeconfig);
 
+// keep the error handlers as the last routes added to the app
 app.use(function (req, res, next) {
-  // the status option, or res.statusCode = 404
-  // are equivalent, however with the option we
-  // get the "status" local available as well
   res.status(404).send("URL " + req.url + " not found");
 });
-
-// error-handling middleware, take the same form
-// as regular middleware, however they require an
-// arity of 4, aka the signature (err, req, res, next).
-// when connect has an error, it will invoke ONLY error-handling
-// middleware.
-
-// If we were to next() here any remaining non-error-handling
-// middleware would then be executed, or if we next(err) to
-// continue passing the error, only error-handling middleware
-// would remain being executed, however here
-// we simply respond with an error page.
-
 app.use(function (err, req, res, next) {
-  // we may use properties of the error object
-  // here and next(err) appropriately, or if
-  // we possibly recovered from the error, simply next().
   console.error(err);
   res.status(500).sent("Internal server error");
 });
